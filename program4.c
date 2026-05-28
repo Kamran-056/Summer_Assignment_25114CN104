72fad560d69556f0a1a11395ad3e05341ebeb9a5
Kamran-056 <kamali967061@gmail.com>
2026-05-28 22:46:32 +0530

    Add files via upload
    Summer Assignment Day 1

---
+++ program4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int n, count =0;
+    printf("enter the number to count digits :" );
+    scanf("%d", &n);
+    while(n!= 0)
+    {
+        count += 1;
+        n /= 10;
+    }
+    printf("The number of digits is %d", count );
+    return 0;
+}
