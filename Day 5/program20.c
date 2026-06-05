f8b03d1a188e5a6f7789f2b5c4c7f46affa7bd7c
Kamran-056 <kamali967061@gmail.com>
2026-06-05 14:14:23 +0530

    Uploaded Day 5

---
+++ Day 5/program20.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n, largestPrime = 1;
+    printf("Enter the number");
+    scanf("%d", &n);
+    for(int i = 2; i <= n; i++)
+    {
+        while(n % i == 0)
+        {
+            largestPrime = i;
+            n /= i;
+        }
+    }
+    printf("Largest Prime Factor = %d", largestPrime);
+    return 0;
+}
