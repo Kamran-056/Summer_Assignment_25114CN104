95df14ec4b7f9f425f6c22f8c4a2cd776b6912e3
Kamran-056 <kamali967061@gmail.com>
2026-06-06 09:52:02 +0530

    Day 6 assignment is Uploaded

---
+++ Program23.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n, count = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while (n > 0)
+    {
+        count += n % 2;
+        n /= 2;
+    }
+    printf("Number od set bits = %d", count);
+    return 0;
+}
