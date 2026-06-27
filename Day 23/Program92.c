77edc4d493cd36d0a69bc07cc30df1cc4dc46fd5
Kamran-056 <kamali967061@gmail.com>
2026-06-27 18:45:28 +0530

    Added Day 23 programs

---
+++ Day 23/Program92.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str[100];
+    int i, j, max = 0, count;
+    char ch;
+    printf("Enter a string: ");
+    scanf("%s", str);
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        count = 1;
+        for(j = i + 1; str[j] != '\0'; j++)
+        {
+            if(str[i] == str[j])
+            count++;
+        }
+        if(count > max)
+        {
+            max = count;
+            ch = str[i];
+        }
+    }
+    printf("Maximum occuring character = %c", ch);
+    return 0;
+}
