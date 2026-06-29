85f1836a4f6d8d4f7d4972bd655b55df076e9fe1
Kamran-056 <kamali967061@gmail.com>
2026-06-29 20:57:28 +0530

    Added Day 28 programs

---
+++ Day 28/Program109.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+struct Library
+{
+    int bookId;
+    char title[50];
+    char author[50];
+};
+int main()
+{
+    struct Library b[100];
+    int n, i;
+    printf("Enter number of books: ");
+    scanf("%d", &n);
+    for(i = 0; i < n; i++)
+    {
+        printf("\nBook %d\n", i + 1);
+        printf("Book ID: ");
+        scanf("%d", &b[i].bookId);
+        printf("Book Title: ");
+        scanf("%s", b[i].title);
+        printf("Author: ");
+        scanf("%s", b[i].author);
+    }
+    printf("\n--- Library Records ---\n");
+    for(i = 0; i < n; i++)
+    {
+        printf("Book ID : %d\n", b[i].bookId);
+        printf("Title : %s\n", b[i].title);
+        printf("Author : %s\n", b[i].author);
+    }
+    return 0;
+}
