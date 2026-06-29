ed4d659b28ccc3cf847906a307405901e00d0490
Kamran-056 <kamali967061@gmail.com>
2026-06-29 18:12:43 +0530

    Added Day 26 programs

---
+++ Day 26/Program104.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int score = 0, answer;
+    printf("Quiz Application\n");
+    printf("\n1. What is the capital of India?\n");
+    printf("1. Mumbai\n2. Delhi\n3. Kolkata\n4. Chennai\n");
+    scanf("%d", &answer);
+    if(answer == 2)
+    score++;
+    printf("\n2. How many days are there in a week?\n");
+    printf("1. 5\n2. 6\n3. 7\n4. 8\n");
+    scanf("%d", &answer);
+    if(answer == 3)
+    score++;
+    printf("\n3. Which language is used for system programming?\n");
+    printf("1. C\n2. HTML\n3. CSS\n4. XML\n");
+    scanf("%d", &answer);
+    if(answer == 1)
+    score++;
+    printf("\nYour Score = %d/3\n", score);
+    return 0;
+}
